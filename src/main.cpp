2d53f92248113430c8d6e871eccbe989c5c38f32
EffessDev <faseeh1080@gmail.com>
2026-07-10 11:30:59 +0530

    get command line args

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,55 @@
 //
 
 #include <iostream>
+#include <unordered_map>
+#include <algorithm>
 
 #include "ProjectScanner.h"
 #include "FileFilter.h"
 
 using namespace std;
 
-int main()
+const std::vector<std::string> FLAGS = {"-i", "-e"};
+
+int main(int argc, char *argv[])
 {
+	// Initialize variables for args
+	std::vector<std::string> includeArgs = {};
+	std::vector<std::string> excludeArgs = {};
+
+	// Parse args
+	std::string currentFlag = "";
+	for (int i = 1; i < argc; i++)
+	{
+		std::string arg = argv[i];
+
+		if (std::ranges::find(FLAGS, arg) != FLAGS.end())
+		{
+			currentFlag = arg;
+			continue;
+		}
+
+		if (currentFlag == "-i")
+		{
+			includeArgs.push_back(arg);
+		}
+		else if (currentFlag == "-e")
+		{
+			excludeArgs.push_back(arg);
+		}
+	}
+
+	// Print args (for development only)
+	for (const auto &arg : includeArgs)
+	{
+		cout << "include arg: " << arg << endl;
+	}
+	for (const auto &arg : excludeArgs)
+	{
+		cout << "exclude arg: " << arg << endl;
+	}
+
+	// Get the relative paths to the files in the project directory (handling .gitignore flies)
 	auto files = ScanProject(std::filesystem::current_path());
 
 	cout << "Files:" << endl
@@ -19,6 +60,7 @@ int main()
 		cout << "file: " << file << endl;
 	};
 
+	// 2nd arg: include patterns, 3rd arg: exclude patterns
 	auto filteredFiles = filterFiles(files, {"**/*.cpp"}, {"tests/"});
 
 	cout << "Filtered Files:" << endl
